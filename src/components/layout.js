1ee622f63b78d45f180264d3132a2cfcb5a69ec5
badape-github <francis@badape.net>
2022-02-25 19:17:08 +0000

    started using grommet

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
-import { Container, Box } from 'theme-ui'
 import { Link } from 'gatsby'
+import { Box } from 'grommet';
 
 const Template = ({ pageTitle, children }) => {
   return (

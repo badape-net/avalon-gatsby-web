acdfc6e283be4b478f1d151012f0b8dd7bc052fd
badape-github <francis@badape.net>
2022-02-25 19:17:39 +0000

    updating grommet usage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import * as React from "react"
 import { graphql } from "gatsby"
 import Pagelayout from "../components/layout"
-import { Container, Card, Text } from 'theme-ui'
 import { GatsbyImage } from "gatsby-plugin-image"
+import { Box, Main, Card, CardBody, CardFooter, CardHeader, Text } from 'grommet';
 
 const IndexPage = ({ data }) => {
   const page = data.directus.Page_by_id
@@ -11,33 +11,33 @@ const IndexPage = ({ data }) => {
 
   return (
     <Pagelayout pageTitle={page.title}>
-      <div>
-        <Container p={4} bg="muted" p={4} sx={{ flex: '1 1 auto' }}>
+      <Main>
+        <Box p={4} bg="muted" sx={{ flex: '1 1 auto' }}>
           <div dangerouslySetInnerHTML={{ __html: page.content }} />
           <GatsbyImage image={page.banner.imageFile.childImageSharp.gatsbyImageData} alt={page.banner.id} />
-        </Container>
+        </Box>
         {
           characters.map((character, i) => (
-            <Container p={4} bg="muted" key={character.name} sx={{ flex: '1 1 auto' }}>
+            <Box p={4} bg="muted" key={character.id} sx={{ flex: '1 1 auto' }}>
               <Card sx={{ maxWidth: 256, }}>
                 <Text sx={{ fontSize: 4, fontWeight: 'bold', }}>
                   {character.name}
                 </Text >
               </Card>
-            </Container>
+            </Box>
           ))
         }
 
         {
           classes.map((character, i) => (
-            <Container bg="muted" key={character.name} p={4} sx={{ flex: '1 1 auto' }}>
+            <Box bg="muted" key={character.id} p={4}>
               <h2>
                 {character.name}
               </h2>
-            </Container>
+            </Box>
           ))
         }
-      </div>
+      </Main>
     </Pagelayout >
   )
 }

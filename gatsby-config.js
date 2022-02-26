179f7c9e822bb9b60aa4508640c17b18012ae9c3
badape-github <francis@badape.net>
2022-02-26 00:00:17 +0000

    got backgound image working

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-image",
+    "gatsby-background-image",  
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     "gatsby-transformer-remark",
